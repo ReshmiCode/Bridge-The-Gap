84a3d56042eba2a3ac518f8c091fb05ad8b24c28
SaloniSS <sxs170015@utdallas.edu>
2020-06-20 02:30:35 -0500

    heroku Deploy

--- website/src/HomePage.js
+++ website/src/HomePage.js
@@ -3,7 +3,11 @@ import './App.css';
 import SignInWithGoogle from './components/SignInWithGoogle';
 import backdrop from './assets/women_in_tech.png'; 
 
+let GLOBAL = require("./global");
+
 function HomePage() {
+  
+  GLOBAL.googleID = "";
 
   const Lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus finibus tortor eget tempus. Curabitur consequat mi quis massa lobortis, a varius ipsum suscipit. Etiam placerat lobortis lacus, a varius turpis luctus quis. Cras augue turpis, vestibulum sed erat nec, vulputate varius enim. Donec pretium, leo nec mollis egestas, nunc tellus sagittis urna, sit amet commodo risus enim tincidunt urna. Vivamus scelerisque lacinia est id venenatis. Maecenas tristique lectus nunc, et rutrum neque mollis vitae. Cras sit amet pulvinar sapien. Vestibulum vel pretium ante, at scelerisque nisl. Mauris feugiat urna id ipsum placerat sollicitudin.";
 
--- website/src/components/SignInWithGoogle.js
+++ website/src/components/SignInWithGoogle.js
@@ -4,14 +4,44 @@ import {GoogleLogin } from 'react-google-login';
 import { withRouter } from 'react-router-dom';
 import {CLIENT_ID} from '../config';
 
+let GLOBAL = require("../global");
+const axios = require("axios").default;
+
 const  SignInWithGoogle = props => {
-    let [signedIn, setSignedIn] = useState(false);
-    let [googleID, setGoogleID] = useState('');
+
+  let [googleProfile, setGoogleProfile] = useState('');
+
+    const newProfile = () => {
+      const newUser = {
+        googleID: googleProfile.googleId,
+        userName: googleProfile.name,
+        profilePic: googleProfile.imageUrl,
+        email: googleProfile.email
+      };
+
+      axios.get(`https://hdt-node-servers.herokuapp.com/NatureDex/api/v1/users/${newUser.googleID}`)
+        .then(function (response) {
+          if (response.data.data.length === 0) {
+            axios
+              .post("https://mlh-bridge-the-gap.wl.r.appspot.com/api/v1/users/", newUser)
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          } 
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    };
   
     const responseGoogleSuccess = (response) => {
-      //console.log(response);
-      setGoogleID(response.Ea);
-      setSignedIn(true);
+      console.log(response);
+      setGoogleProfile(response.profileObj)
+      GLOBAL.googleID = response.googleId;
+      newProfile();
       props.history.push("/profile");
     }
   
