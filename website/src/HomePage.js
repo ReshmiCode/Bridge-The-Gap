4cab21690135e551498518117c34ed0887780474
SaloniSS <sxs170015@utdallas.edu>
2020-06-19 22:52:03 -0500

    Set Up Base  Routes

---
+++ website/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Bridge The Gap </h1>
+        <p> Blah Blah  Blah </p>
+        <p> Sign in using Google </p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
