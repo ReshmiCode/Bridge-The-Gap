a87e9adaeccf50a9d5522aa7ac77b1c043178522
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-20 17:40:28 -0500

    move to component

---
+++ website/src/components/ProfileBar.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Avatar, IconButton, TextField } from "@material-ui/core";
+import {
+  Create,
+  PhotoCamera,
+  LinkedIn,
+  Twitter,
+  GitHub,
+} from "@material-ui/icons";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+
+function ProfileBar() {
+  const [selected, setSelected] = useState(false);
+  const sampleBio =
+    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.";
+
+  return (
+    <div className="profile-bar">
+      <div style={{ flex: 1 }}>
+        <Avatar
+          alt="Profile"
+          src="https://image.shutterstock.com/image-vector/social-member-vector-icon-person-260nw-1139787308.jpg"
+          style={{ height: 160, width: 160 }}
+        />
+        <IconButton>
+          <PhotoCamera />
+        </IconButton>
+      </div>
+      <div style={{ flex: 2 }}>
+        <h2>Reshmi Ranjith</h2>
+        {!selected ? (
+          <p style={{ fontSize: 20 }}>{sampleBio}</p>
+        ) : (
+          <TextField
+            multiline
+            rowsMax={4}
+            id="outlined-basic"
+            defaultValue={sampleBio}
+            variant="outlined"
+            labelWidth={60}
+            fullWidth
+          />
+        )}
+        <ToggleButton
+          value="check"
+          selected={selected}
+          onChange={() => {
+            setSelected(!selected);
+          }}
+        >
+          <Create fontSize="small" />
+        </ToggleButton>
+      </div>
+      <div style={{ flex: 1 }}>
+        <p>Social Links:</p>
+        <IconButton>
+          <LinkedIn />
+        </IconButton>
+        <IconButton>
+          <Twitter />
+        </IconButton>
+        <IconButton>
+          <GitHub />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileBar;
--- website/src/screens/Profile.js
+++ website/src/screens/Profile.js
@@ -1,75 +1,11 @@
-import React, { useState } from "react";
-import { Avatar, IconButton, TextField } from "@material-ui/core";
-import {
-  Create,
-  PhotoCamera,
-  LinkedIn,
-  Twitter,
-  GitHub,
-} from "@material-ui/icons";
-import ToggleButton from "@material-ui/lab/ToggleButton";
+import React from "react";
+import ProfileBar from "../components/ProfileBar";
 import "./Profile.css";
 
-function Profile() {
-  const [selected, setSelected] = useState(false);
-  const sampleBio =
-    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.";
-
+export default function Profile() {
   return (
     <div className="App">
-      <div className="profile-bar">
-        <div style={{ flex: 1 }}>
-          <Avatar
-            alt="Profile"
-            src="https://image.shutterstock.com/image-vector/social-member-vector-icon-person-260nw-1139787308.jpg"
-            style={{ height: 160, width: 160 }}
-          />
-          <IconButton>
-            <PhotoCamera />
-          </IconButton>
-        </div>
-        <div style={{ flex: 2 }}>
-          <h2>Reshmi Ranjith</h2>
-          {!selected ? (
-            <p style={{ fontSize: 20 }}>{sampleBio}</p>
-          ) : (
-            <form noValidate autoComplete="off">
-              <TextField
-                multiline
-                rowsMax={4}
-                id="outlined-basic"
-                defaultValue={sampleBio}
-                variant="outlined"
-                labelWidth={60}
-                fullWidth
-              />
-            </form>
-          )}
-          <ToggleButton
-            value="check"
-            selected={selected}
-            onChange={() => {
-              setSelected(!selected);
-            }}
-          >
-            <Create fontSize="small" />
-          </ToggleButton>
-        </div>
-        <div style={{ flex: 1 }}>
-          <p>Social Links:</p>
-          <IconButton>
-            <LinkedIn />
-          </IconButton>
-          <IconButton>
-            <Twitter />
-          </IconButton>
-          <IconButton>
-            <GitHub />
-          </IconButton>
-        </div>
-      </div>
+      <ProfileBar />
     </div>
   );
 }
-
-export default Profile;
