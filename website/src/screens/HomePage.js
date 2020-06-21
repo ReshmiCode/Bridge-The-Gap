2f853af37349c6d3de014f90dba486c4c6f33a79
Megan Tran <meganjtran@gmail.com>
2020-06-21 06:37:35 -0600

    oh god i broke the button

--- website/src/screens/HomePage.js
+++ website/src/screens/HomePage.js
@@ -10,8 +10,8 @@ function HomePage() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus finibus tortor eget tempus. Curabitur consequat mi quis massa lobortis, a varius ipsum suscipit. Etiam placerat lobortis lacus, a varius turpis luctus quis. Cras augue turpis, vestibulum sed erat nec, vulputate varius enim. Donec pretium, leo nec mollis egestas, nunc tellus sagittis urna, sit amet commodo risus enim tincidunt urna. Vivamus scelerisque lacinia est id venenatis. Maecenas tristique lectus nunc, et rutrum neque mollis vitae. Cras sit amet pulvinar sapien. Vestibulum vel pretium ante, at scelerisque nisl. Mauris feugiat urna id ipsum placerat sollicitudin.";
 
   const button = {
-    width: 120,
-    height: 45,
+    width: 200,
+    height: 50,
     margin: 10,
     borderRadius: 15,
     backgroundColor: "#ffa090",
