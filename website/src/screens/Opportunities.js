4ff4c93cf96a3e0dfaa39ab79cba289443a8b568
SaloniSS <sxs170015@utdallas.edu>
2020-06-20 19:49:42 -0500

    Base for opportunities

--- website/src/screens/Opportunities.js
+++ website/src/screens/Opportunities.js
@@ -1,9 +1,18 @@
 import React from "react";
+import CardList from "../components/CardList";
 
 function Opportunities() {
   return (
     <div className="App">
       <h2> Opportunities </h2>
+      <h3>Organizations</h3>
+      <CardList />
+      <h3>Scholarships</h3>
+      <CardList />
+      <h3>Conferences</h3>
+      <CardList />
+      <h3>Other Resources</h3>
+      <CardList />
     </div>
   );
 }
