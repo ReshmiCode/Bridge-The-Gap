dfc62f29166a74de9144052af5d4c22d3300eada
SaloniSS <sxs170015@utdallas.edu>
2020-06-20 20:36:54 -0500

    Add default to resources

--- server/models/resource_model.js
+++ server/models/resource_model.js
@@ -17,11 +17,13 @@ const ResourceSchema = new mongoose.Schema({
     },
     
     image: {
-        type: String
+        type: String,
+        default: 'https://www.protexinvet.com/userfiles/image/cute-2500929_1920_(1).jpg'
     },
 
     link: {
-        type: String
+        type: String,
+        default: 'http://localhost:3000/'
     },
 });
 
