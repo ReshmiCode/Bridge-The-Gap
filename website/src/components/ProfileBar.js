0ec53a670685f5c3338d53845c1657519a4e1025
SaloniSS <sxs170015@utdallas.edu>
2020-06-20 23:33:13 -0500

    Add profile info

--- website/src/components/ProfileBar.js
+++ website/src/components/ProfileBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Avatar, IconButton, TextField, Button } from "@material-ui/core";
 import {
   Create,
@@ -8,9 +8,23 @@ import {
   GitHub,
 } from "@material-ui/icons";
 import ToggleButton from "@material-ui/lab/ToggleButton";
+const axios = require("axios").default;
+let GLOBAL = require("../global");
 
 function ProfileBar() {
   const [selected, setSelected] = useState(false);
+  let [info, setInfo] = useState(["GCP"]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        `https://mlh-bridge-the-gap.wl.r.appspot.com/api/v1/users/${GLOBAL.googleID}`
+      );
+      setInfo(result.data.data[0]);
+    }
+    fetchData();
+  }, []);
+
   const sampleBio =
     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.";
 
@@ -19,7 +33,7 @@ function ProfileBar() {
       <div style={{ flex: 1 }}>
         <Avatar
           alt="Profile"
-          src="https://image.shutterstock.com/image-vector/social-member-vector-icon-person-260nw-1139787308.jpg"
+          src={info.profilePic}
           style={{ height: 160, width: 160 }}
         />
         <IconButton>
@@ -27,15 +41,15 @@ function ProfileBar() {
         </IconButton>
       </div>
       <div style={{ flex: 2 }}>
-        <h2>Reshmi Ranjith</h2>
+        <h2>{info.userName}</h2>
         {!selected ? (
-          <p style={{ fontSize: 20 }}>{sampleBio}</p>
+          <p style={{ fontSize: 20 }}>{info.bio}</p>
         ) : (
           <TextField
             multiline
             rowsMax={4}
             id="outlined-basic"
-            defaultValue={sampleBio}
+            defaultValue={info.bio}
             variant="outlined"
             labelWidth={60}
             fullWidth
