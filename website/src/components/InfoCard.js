5e445bf571cf859941c23d5bee335175a137d448
Megan Tran <meganjtran@gmail.com>
2020-06-20 22:39:43 -0600

    fixed opporunity cards and added pic to home

--- website/src/components/InfoCard.js
+++ website/src/components/InfoCard.js
@@ -1,50 +1,62 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardContent,
   Typography,
   GridList,
   GridListTile,
-  CardMedia
+  CardMedia,
+  shadows,
 } from "@material-ui/core";
 
 const InfoCard = (props) => {
+  const resource = props.resource;
+  console.log(resource);
 
-    const resource = props.resource;
-    console.log(resource);
-    
-    const flexContainer = {
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        overflow: "hidden",
-    };
-    const gridList = {
-        flexWrap: "nowrap",
-        transform: "translateZ(0)",
-    };
+  const flexContainer = {
+    display: "noflex",
+    flexWrap: "nowrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+  };
 
-    return (
-        <GridListTile style={{ height: null , width:"25%"}}>
-            <Card>
-                <CardContent>
-                    <Typography variant="h5"> {resource.title} </Typography>
-                    {
-                        resource.image ? 
-                        <img src={resource.image} />:
-                        <img src="https://image.shutterstock.com/image-photo/two-small-rabbits-isolated-on-260nw-795213922.jpg" />
-                    }
-                    <Typography> {resource.content} </Typography>
-                    {
-                        resource.link && 
-                        <button>
-                            <a  href={resource.link} target="_blank">Know More</a>
-                        </button>
-                    }
-                </CardContent>
-            </Card>
-        </GridListTile>
-    );
+  const gridList = {
+    flexWrap: "nowrap",
+    transform: "translateZ(0)",
+    height: null,
+    width: "100%",
+  };
+
+  const card = {
+    borderRadius: 20,
+    margin: 10,
+    boxShadow: 3,
+  };
+
+  return (
+    <div style={flexContainer}>
+      <GridListTile style={gridList}>
+        <Card style={card}>
+          <CardContent>
+            <Typography variant="h5"> {resource.title} </Typography>
+            {resource.image ? (
+              <img src={resource.image} />
+            ) : (
+              <img src="https://image.shutterstock.com/image-photo/two-small-rabbits-isolated-on-260nw-795213922.jpg" />
+            )}
+            <Typography> {resource.content} </Typography>
+            {resource.link && (
+              <button>
+                <a href={resource.link} target="_blank">
+                  Know More
+                </a>
+              </button>
+            )}
+          </CardContent>
+        </Card>
+      </GridListTile>
+    </div>
+  );
 };
 
 export default InfoCard;
--- website/src/components/OpportunityList.js
+++ website/src/components/OpportunityList.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardContent,
@@ -10,37 +10,39 @@ import InfoCard from "./InfoCard";
 const axios = require("axios").default;
 
 const OpportunityList = (props) => {
+  let [resources, setResources] = useState([]);
 
-    let [resources, setResources] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const getResources = await axios(
+        `https://mlh-bridge-the-gap.wl.r.appspot.com/api/v1/resources/category/${props.category}`
+      );
+      setResources(getResources.data.data.reverse());
+    }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        async function fetchData() {
-          const getResources = await axios(`https://mlh-bridge-the-gap.wl.r.appspot.com/api/v1/resources/category/${props.category}`);
-          setResources(getResources.data.data.reverse());
-        }
-        fetchData();
-    }, []);
-    
-    const flexContainer = {
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        overflow: "hidden",
-    };
-    const gridList = {
-        flexWrap: "nowrap",
-        transform: "translateZ(0)",
-    };
+  const flexContainer = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+  };
+  const gridList = {
+    flexWrap: "wrap",
+    transform: "translateZ(0)",
+    justifyContent: "space-around",
+  };
 
-    return (
-        <div style={flexContainer}>
-            <GridList style={gridList}>
-                { resources.map(function (resource, i) {
-                    return(<InfoCard resource={resource}/>)
-                })}
-            </GridList>
-        </div>
-    );
+  return (
+    <div style={flexContainer}>
+      <GridList style={gridList}>
+        {resources.map(function (resource, i) {
+          return <InfoCard resource={resource} />;
+        })}
+      </GridList>
+    </div>
+  );
 };
 
 export default OpportunityList;
