da16dd41daa542f39f70ed2c3bb5ac9e105c27f8
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-20 22:47:04 -0500

    qna cleanup

--- website/src/screens/QnA.js
+++ website/src/screens/QnA.js
@@ -1,33 +1,40 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import SearchBar from "search-bar-react";
 
 const direction = {
-  flexdirection: "row"
+  flexdirection: "row",
 };
 const size = {
-  margin: 40
+  margin: 40,
+  backgroundColor: "#ffa899",
 };
 
 function QnA() {
   return (
     <div className="App">
-      <h2> Q+A </h2>
-      <p>Frequently Asked Questions</p>
+      <h2>Frequently Asked Questions</h2>
+      <SearchBar
+        onChange={(text) => console.log(text)}
+        onFocus={() => console.log("focused")}
+        size="large"
+        width="100%"
+        autoFocus
+        placeholder="Search..."
+        onClear={() => console.log("closed")}
+      />
 
-      <p>Search</p>      
-      
       <div style={direction}>
-      <Button variant="contained" color="#C4C4C4" disableElevation style={size}>
-        General
-      </Button>
-      <Button variant="contained" color="#C4C4C4" disableElevation style={size}>
-        Opportunity
-      </Button>
-      <Button variant="contained" color="#C4C4C4" disableElevation style={size}>
-        Community
-      </Button>
-      </div>    
-
+        <Button variant="contained" style={size}>
+          General
+        </Button>
+        <Button variant="contained" style={size}>
+          Opportunity
+        </Button>
+        <Button variant="contained" style={size}>
+          Community
+        </Button>
+      </div>
     </div>
   );
 }
